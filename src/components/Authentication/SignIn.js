ebb09a88484a05898d312c25001b71ea8346e354
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-05-05 22:04:30 -0400

    styling for signup and signin

--- src/components/Authentication/SignIn.js
+++ src/components/Authentication/SignIn.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { auth } from '../../firebase';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useHistory } from 'react-router-dom';
-
+import './SignIn.css';
 const SignIn = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
