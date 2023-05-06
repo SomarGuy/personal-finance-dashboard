a49fb31d77bc1d0f1a8c7ecf7c7d2c6d2b322717
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-05-05 20:57:05 -0400

    implement SignIn component

---
+++ src/Authentication/SignIn.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { auth } from '../firebase';
+
+const SignIn = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error('Error signing in:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign In</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Sign In</button>
+    </form>
+  );
+};
+
+export default SignIn;
---
+++ src/Authentication/SignUp.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { auth } from '../firebase';
+
+const SignUp = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await auth.createUserWithEmailAndPassword(email, password);
+      setEmail('');
+      setPassword('');
+    } catch (error) {
+      console.error('Error signing up:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign Up</h2>
+      <input
+        type="email"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
