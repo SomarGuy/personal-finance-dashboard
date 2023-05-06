17c4b25bef900415b8ad2096428e6804b33af662
Somaro <106792137+SomarGuy@users.noreply.github.com>
2023-05-05 20:58:16 -0400

    update file to include react router and protected routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { auth } from './firebase';
+import SignIn from './components/Authentication/SignIn';
+import SignUp from './components/Authentication/SignUp';
+import Dashboard from './components/Dashboard/Dashboard';
 
-function App() {
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      auth.currentUser ? <Component {...props} /> : <Redirect to="/signin" />
+    }
+  />
+);
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <PrivateRoute path="/dashboard" component={Dashboard} />
+        <Redirect from="/" to="/dashboard" />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
